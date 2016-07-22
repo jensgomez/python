a96fbaefde40f6f011b717033321706496496034
jensgomez <jerald.daniel@gmail.com>
2016-07-21 21:53:36 -0400

    Add files via upload

---
+++ untitled16.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 21 19:25:51 2016
+
+@author: Jens
+"""
+
+import scipy as sp
+import matplotlib.pyplot as plt
+
+class extrema:
+    def __init__(self,f,a,b):
+        self.f = f
+        self.a = a
+        self.b = b
+        
+    def roots(self):
+        root = sp.optimize.brentq(self.f,self.a,self.b)
+        n = 100
+        x = sp.linspace(self.a,self.b,n)
+        func = sp.zeros(n)
+        for i in range(0,n):
+            func[i] = self.f(x[i])
+
+        plt.plot(root,0, 'ro')
+        plt.axhline(y=0)
+        plt.plot(x,func)
+        print("root between x1 = {0:6.4f} and x2 = {1:6.4f} is at xr = {2:6.4f}".format(self.a,self.b,root))
+        return root
+
+
+def f1(x):
+    return sp.cos(x)
+    
+    
+def f2(x):
+    return sp.sin(x)*sp.log(x)
+    
+    
+
+# minmax1 = extrema(f1,2*sp.pi,3*sp.pi)
+# r1 = minmax1.roots()
+
+
+minmax2 = extrema(f2,2,4)
+r2 = minmax2.roots()
